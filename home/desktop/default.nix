888f2e0135437a8cfae1cb6a03307a6f45727679
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-10-03 10:12:56 +0200

    buggy code

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,3 +1,13 @@
-{...}: {
+{pkgs, lib, ...}: {
   imports = [  ];
+
+  home.packages = with pkgs; [
+    dconf
+  ];
+
+  # Use home.activation to apply dconf settings
+  home.activation.dconf = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    #!/bin/sh
+    dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+  '';
 }
