c5e1943ee1892fdd0900d22264dc66a657b516fb
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-10-03 10:17:06 +0200

    'revert'

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -1,13 +1,3 @@
 {pkgs, lib, ...}: {
   imports = [  ];
-
-  home.packages = with pkgs; [
-    dconf
-  ];
-
-  # Use home.activation to apply dconf settings
-  home.activation.dconf = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    #!/bin/sh
-    dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-  '';
 }
